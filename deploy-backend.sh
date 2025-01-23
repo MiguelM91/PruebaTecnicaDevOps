07bce967074ed96a07f29d56320f02f4430d5f0f
MiguelM91 <miguelarturomunozs@gmail.com>
2025-01-23 08:08:18 -0500

    :construction: Add missing dependencies

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -6,3 +6,16 @@ docker build \
           --build-arg RDS_PASSWORD=adm1n123*$ \
           --build-arg S3_BUCKET_NAME=front-end-storage \
           -t todo-app .
+
+# Comando para correr el backend
+
+docker rm -f todo-app-container
+
+docker run -d -p 5000:5000 \
+          --name todo-app-container \
+          -e RDS_INSTANCE_IDENTIFIER=todo-db-instance \
+          -e RDS_DB_NAME=todo_db \
+          -e RDS_USERNAME=miguel \
+          -e RDS_PASSWORD=adm1n123*$ \
+          -e S3_BUCKET_NAME=front-end-storage \
+          todo-app
